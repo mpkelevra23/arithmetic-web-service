5c3ec22abe549761893c8b70a7e5090208d64947
mpkelevra23 <mpkelevra23@gmal.com>
2024-12-23 12:51:52 +0400

    Formatted the test handler_test.go

--- tests/handler_test.go
+++ tests/handler_test.go
@@ -33,110 +33,79 @@ func TestCalculateHandler(t *testing.T) {
 		expectedBody   map[string]string
 	}{
 		{
-			name:   "Valid Expression",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expression": "1 + 2 * 3",
-			},
+			name:           "Valid Expression",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expression": "1 + 2 * 3"},
 			expectedStatus: http.StatusOK,
-			expectedBody: map[string]string{
-				"result": "7",
-			},
+			expectedBody:   map[string]string{"result": "7"},
 		},
 		{
-			name:   "Invalid Characters",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expression": "1 + a",
-			},
+			name:           "Invalid Characters",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expression": "1 + a"},
 			expectedStatus: http.StatusUnprocessableEntity,
-			expectedBody: map[string]string{
-				"error": errors.ErrInvalidInput,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrInvalidInput},
 		},
 		{
-			name:   "Division by Zero",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expression": "10 / 0",
-			},
+			name:           "Division by Zero",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expression": "10 / 0"},
 			expectedStatus: http.StatusUnprocessableEntity,
-			expectedBody: map[string]string{
-				"error": errors.ErrDivisionByZero,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrDivisionByZero},
 		},
 		{
-			name:   "Missing Expression Field",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expr": "1 + 2",
-			},
+			name:           "Missing Expression Field",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expr": "1 + 2"},
 			expectedStatus: http.StatusUnprocessableEntity,
-			expectedBody: map[string]string{
-				"error": errors.ErrMissingField,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrMissingField},
 		},
 		{
-			name:   "Empty Expression",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expression": "",
-			},
+			name:           "Empty Expression",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expression": ""},
 			expectedStatus: http.StatusUnprocessableEntity,
-			expectedBody: map[string]string{
-				"error": errors.ErrMissingField,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrMissingField},
 		},
 		{
 			name:           "Unsupported HTTP Method",
 			method:         http.MethodGet, // Используем неподдерживаемый метод
 			payload:        nil,            // Нет тела запроса
 			expectedStatus: http.StatusMethodNotAllowed,
-			expectedBody: map[string]string{
-				"error": errors.ErrUnsupportedMethod,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrUnsupportedMethod},
 		},
 		{
 			name:           "Malformed JSON",
 			method:         http.MethodPost,
 			payload:        `{"expression": "1 + 2",`, // Неправильный JSON
 			expectedStatus: http.StatusBadRequest,
-			expectedBody: map[string]string{
-				"error": errors.ErrMalformedJSON,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrMalformedJSON},
 		},
 		{
-			name:   "Expression Too Long",
-			method: http.MethodPost,
-			payload: map[string]string{
-				"expression": generateLongExpression(1001), // Предполагаем, что 1001 символ превышает лимит
-			},
+			name:           "Expression Too Long",
+			method:         http.MethodPost,
+			payload:        map[string]string{"expression": generateLongExpression(1001)}, // Предполагаем, что 1001 символ превышает лимит
 			expectedStatus: http.StatusUnprocessableEntity,
-			expectedBody: map[string]string{
-				"error": errors.ErrTooLongExpression,
-			},
+			expectedBody:   map[string]string{"error": errors.ErrTooLongExpression},
 		},
 		{
 			name:           "Trigger Internal Server Error by Header",
 			method:         http.MethodPost,
 			payload:        map[string]string{"expression": "1+1"},
 			expectedStatus: http.StatusInternalServerError,
-			expectedBody: map[string]string{
-				"error": "Internal Server Error",
-			},
+			expectedBody:   map[string]string{"error": "Internal Server Error"},
 		},
 		{
 			name:           "Trigger Internal Server Error by Long Expression",
 			method:         http.MethodPost,
 			payload:        map[string]string{"expression": generateLongExpression(800)},
 			expectedStatus: http.StatusInternalServerError,
-			expectedBody: map[string]string{
-				"error": "Expression length triggered server error",
-			},
+			expectedBody:   map[string]string{"error": "Expression length triggered server error"},
 		},
 	}
 
 	for _, tt := range tests {
+		tt := tt // Захват переменной для параллельного выполнения
 		t.Run(tt.name, func(t *testing.T) {
 			var reqBody []byte
 			var err error
